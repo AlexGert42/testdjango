f79a1ebe873a9f202e32e107238dac189b425a9e
MrAlexGert <alex.gert1812@.gmail.com>
2021-10-21 18:32:12 +0300

    'admin_create'

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import *
+
+
+
+class HumanAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'time_update', 'photo', 'is_public', 'cat')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content', 'id')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('name',)
+    search_fields = ('name', 'id')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Human, HumanAdmin)
--- base/models.py
+++ base/models.py
@@ -8,15 +8,34 @@ from django.urls import reverse
 
 
 class Human(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_public = models.BooleanField(default=True)
+    title = models.CharField(max_length=255, verbose_name='Название')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True, verbose_name='Фото')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    is_public = models.BooleanField(default=True, verbose_name='Опубликован')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Сотрудники'
+        verbose_name_plural = 'Сотрудники'
+        ordering = ['-time_create', 'title']
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Название категории')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категории'
+        # ordering = ['-time_create', 'name']
